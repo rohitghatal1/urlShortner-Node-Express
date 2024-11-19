0631b1fbea718d56208ea6429d5a3460e25f0e32
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-11-19 09:49:48 +0545

    added ant desing

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export default function Homepage() {
   return (
     <div>
-      <div className="homePage px-2 py-3 bg-red-500 text-lime-200">
-        <h2 className="text-center">Home Page</h2>
+      <div className="homePage px-2 py-3 border-b-2 border-gray-500">
+        <h2 className="text-center">Url Shortner</h2>
+        
       </div>
     </div>
   )
