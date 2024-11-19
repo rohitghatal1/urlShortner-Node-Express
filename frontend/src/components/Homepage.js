3b8330c4bd5549674bb663c6706cf5738844f1a5
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-11-19 08:30:39 +0545

    initialized tailwind css for frontend

---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div>
+      <div className="homePage px-2 py-3 bg-red-500 text-lime-200">
+        <h2 className="text-center">Home Page</h2>
+      </div>
+    </div>
+  )
+}
