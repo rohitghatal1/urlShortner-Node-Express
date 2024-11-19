878f51bdf8c549ab77580cdb6482c662c3387ea7
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-11-19 20:32:44 +0545

    added form

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -6,7 +6,12 @@ export default function Homepage() {
       <div className="homePage px-2 py-3 border-b-2 border-gray-500">
         <h2 className="text-center">Url Shortner</h2>
         <section className="urlSection">
-          
+          <div className="form">
+            <form action="">
+              <label htmlFor="url">Enter url to short</label>
+              <input type="text" placeholder='long url' />
+            </form>
+          </div>
         </section>
       </div>
     </div>
