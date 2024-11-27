624818bcccc6e80dc4d14f2507b3c34ba1c526df
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-11-27 22:12:43 +0545

    updated code

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Form } from "antd"
 
 export default function Homepage() {
   return (
     <div>
-      <div className="homePage px-2 py-3 border-b-2 border-gray-500">
+      <div className="homePage px-2 py-3 flex justify-center items-center">
         <h2 className="text-center">Url Shortner</h2>
         <section className="urlSection">
           <div className="form">
-            <form action="">
-              <label htmlFor="url">Enter url to short</label>
-              <input type="text" placeholder='long url' />
-            </form>
+            <Form layout='vertical'>
+              <Form.Item label="Long url">
+
+              </Form.Item>
+            </Form>
           </div>
         </section>
       </div>
