966a85ecb1562540ae010284bb6a38e8e23ee4e1
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-11-29 21:48:12 +0545

    added reac router package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Homepage from './components/Homepage'
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
     <div>
-      <Homepage/>
+
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      </Routes>
     </div>
   )
 }
