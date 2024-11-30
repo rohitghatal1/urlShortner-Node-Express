c341fe99d2bbbc2a2ddda3b589b365ba622fcd46
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-11-30 09:15:02 +0545

    added navigator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Homepage from './components/Homepage'
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <div>
-
+      <Navbar />
       <Routes>
         <Route path='/' element={<Homepage />} />
       </Routes>
