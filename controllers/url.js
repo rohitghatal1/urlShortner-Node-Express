df411d103763815a259823f9c5d8ce2172154614
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-12-02 20:47:27 +0545

    added axios

--- controllers/url.js
+++ controllers/url.js
@@ -1,17 +1,18 @@
 const shortid = require("shortid");
 const URL = require('../models/url')
+import axios from "axios";
 
 async function generateShortURL(req, res) {
     const body = req.body;
-    if(!body.url) return res.status(400).json({error: "url is required"})
+    if (!body.url) return res.status(400).json({ error: "url is required" })
     const shortId = shortid();
     await URL.create({
         shortId: shortId,
         redirectURL: body.url,
-        visitHistory: [], 
+        visitHistory: [],
     });
 
-    return res.json({id: shortId})
+    return res.json({ id: shortId })
 }
 
 module.exports = {
