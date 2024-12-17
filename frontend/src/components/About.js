cc2b443afe6d2646fa35741460d5974d3873ce39
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-12-17 20:42:14 +0545

    updated styles

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            <div>
-                <h2>
+        <div className='flex flex-col gap-2 p-4'>
+            <div className='border rounded-md bg-gray-300 p-2'>
+                <h2 className='text-center font-semibold text-lg'>
                     About us page
                 </h2>
                 <p>This is aboout page</p>
