489e585f0c4eb057bb7ef00eac80f00eebc86345
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-12-17 20:52:39 +0545

    changes

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,11 +7,11 @@ export default function About() {
                 <h2 className='text-center font-semibold text-lg text-gray-700 py-2'>
                     About us page
                 </h2>
-                <div className='text-gray-400 py-1 mt-4'>
-                    <div>
+                <div className='text-gray-400 py-1 mt-4 flex items-center justify-around gap-3'>
+                    <div className='border rounded-md p-3'>
                         <h3>Left side content</h3>
                     </div>
-                    <div>
+                    <div className='border rounded-md p-3'>
                         <h3>Right side content</h3>
                     </div>
                 </div>
